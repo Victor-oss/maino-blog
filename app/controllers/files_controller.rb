4e5f9d467f26ccec4d6fabf41b2cf72cb263854e
Victor-oss <victorio.lazaro15@gmail.com>
2024-04-21 16:32:17 -0300

    Tratando erros e acertos do upload de arquivos com posts

--- app/controllers/files_controller.rb
+++ app/controllers/files_controller.rb
@@ -2,8 +2,21 @@ class FilesController < ApplicationController
     def upload
         uploaded_file = params[:file]
         text_data = File.read(uploaded_file)
-        parsed_data = JSON.parse(text_data)
-        PostJob.perform_later(parsed_data, Current.user.id)
-        redirect_to posts_path
+        if valid_json?(text_data)
+            parsed_data = JSON.parse(text_data)
+            PostJob.perform_later(parsed_data, Current.user.id)
+            redirect_to posts_path, notice: "Se o arquivo estava formatado corretamente, os posts serão criados em instantes"  
+        else
+            redirect_to post_path, alert: "Arquivo no formato inválido ou não formatado do jeito correto" 
+        end
+    end
+
+    private
+
+    def valid_json?(json)
+        JSON.parse(json)
+            true
+        rescue JSON::ParserError, TypeError => e
+            false
     end
 end
